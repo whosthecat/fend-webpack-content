68a5f3c522d84840cebe4692c7a14afc4fb07743
whosthecat <79358806+whosthecat@users.noreply.github.com>
2021-04-11 13:51:18 +0200

    Final touch

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,8 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: './src/client/index.js',
@@ -30,6 +31,7 @@ module.exports = {
                // Automatically remove all unused webpack assets on rebuild
                cleanStaleWebpackAssets: true,
                protectWebpackAssets: false
-       })
+       }),
+       new BundleAnalyzerPlugin()
     ]
 }
